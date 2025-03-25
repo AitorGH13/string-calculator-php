bff3898fdb7e680a2a3e8e5371126bf97352e237
AitorGH13 <arrarasaitor@gmail.com>
2025-03-25 01:10:55 +0100

    Progreso

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,5 +4,30 @@ namespace Deg540\StringCalculatorPHP;
 
 class StringCalculator
 {
-    // TODO: String Calculator Kata
+    public function add(string $numbers): int
+    {
+        if (strpos($numbers, '//') === 0) {
+            $delimiter = $numbers[2];
+            $numbers = substr($numbers, 4);
+        } else {
+            $delimiter = ",|\n";
+        }
+        $numbers = preg_replace("/[$delimiter]/", ",", $numbers);
+        if (str_contains($numbers, ',')) {
+            $numbersArray = explode(',', $numbers);
+            $negatives = array_filter($numbersArray, fn($number) => (int)$number < 0);
+            if (!empty($negatives)) {
+                throw new \InvalidArgumentException(
+                    "negativos no soportados: " . implode(", ", $negatives)
+                );
+            }
+            $numbersArray = array_filter($numbersArray, fn($number) => (int)$number <= 1000);
+            return array_sum($numbersArray);
+        }
+        if (empty($numbers)) {
+            return 0;
+        }
+
+        return $numbers;
+    }
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -6,8 +6,75 @@ namespace Deg540\StringCalculatorPHP\Test;
 
 use Deg540\StringCalculatorPHP\StringCalculator;
 use PHPUnit\Framework\TestCase;
+use InvalidArgumentException;
 
-final class StringCalculatorTest extends TestCase
+class StringCalculatorTest extends TestCase
 {
-    // TODO: String Calculator Kata Tests
+    private StringCalculator $stringCalculator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->stringCalculator = new StringCalculator();
+    }
+    /**
+     * @test
+     */
+    public function givenEmptyStringReturnsZero(): void
+    {
+        $this->assertEquals(0, $this->stringCalculator->add(""));
+    }
+    /**
+     * @test
+     */ 
+    public function givenSingleNumberReturnsSameNumber(): void
+    {
+        $stringCalculator = new StringCalculator();
+        $this->assertEquals(1, $this->stringCalculator->add("1"));
+    }
+    /**
+     * @test
+     */
+    public function givenNumbersSeparatedByCommasReturnsSumOfNumbers(): void
+    {
+        $this->assertEquals(6, $this->stringCalculator->add("1,2,3"));
+    }
+    /**
+     * @test
+     */
+    public function givenNumbersSeparatedByCommasAndLineBreakReturnsSumOfNumbers(): void
+    {
+        $this->assertEquals(6, $this->stringCalculator->add("1\n2,3"));  
+    }
+    /**
+     * @test
+     */
+    public function givenNumbersSeparatedByCustomDelimiterReturnsSumOfNumbers(): void
+    {
+        $this->assertEquals(6, $this->stringCalculator->add("//;\n1;2;3"));
+    }
+    /**
+     * @test
+     */
+    public function givenNegativeNumbersThrowsExceptionWithAllNegativeNumbers(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("negativos no soportados: -1, -2, -3");
+        $this->stringCalculator->add("1,-1,-2,-3"); 
+    }
+    /**
+     * @test
+     */
+    public function givenNumbersGreaterThan1000AreIgnoredAndReturnsSumOfRemainingNumbers(): void
+    {
+        $this->assertEquals(6, $this->stringCalculator->add("1,1001,2,2000,3")); 
+    }
+    /**
+     * @test
+     */
+    public function givenNumbersSeparatedByCustomsDelimitersReturnsSumOfNumbers(): void
+    {
+        $this->assertEquals(6, $this->stringCalculator->add("//***\n13")); 
+    }
 }
+ 
